02768b7a2043f1adc3b0d6bba498e1191164b898
Joshua Sutcliffe <joshua_sutcliffe@outlook.com>
2023-05-12 07:33:04 +0800

    Addressed requirements of the porfolio

--- angry.py
+++ angry.py
@@ -1,19 +1,10 @@
 import time
-from blinkable import Blinkable
 from smiley import Smiley
 
 
-class Happy(Smiley, Blinkable):
-    """
-    Happy is a subclass of Smiley and of Blinkable.
-
-    Note that Blinkable is an interface (an abstract base
-    class that only contains an abstract method). By subclassing
-    Blinkable, this class promises to implement the abstract
-    method.See {meth:blink} below.
-    """
+class Angry(Smiley):
     def __init__(self):
-        super().__init__()
+        super().__init__(complexion=self.RED)
 
         self.draw_mouth()
         self.draw_eyes()
@@ -22,7 +13,7 @@ class Happy(Smiley, Blinkable):
         """
         Method that draws the mouth on the standard faceless smiley.
         """
-        mouth = [41, 46, 50, 51, 52, 53]
+        mouth = [41, 42, 43, 44, 45, 46]
         for pixel in mouth:
             self.pixels[pixel] = self.BLANK
 
@@ -31,7 +22,7 @@ class Happy(Smiley, Blinkable):
         Method that draws the eyes (open or closed) on the standard smiley.
         :param wide_open: True if eyes opened, False otherwise
         """
-        eyes = [10, 13, 18, 21]
+        eyes = [17, 22, 25, 26, 29, 30]
         for pixel in eyes:
             self.pixels[pixel] = self.BLANK if wide_open else self.complexion()
 
@@ -40,9 +31,15 @@ class Happy(Smiley, Blinkable):
         Make the happy smiley blink once with a certain delay (in s).
         This is the implementation of the abstract method from the
         Blinkable abstract class.
-
         :param delay: Delay in seconds
         """
+        self.draw_eyes(wide_open=False)
+        self.show()
+        time.sleep(delay)
+        self.draw_eyes(wide_open=True)
+        self.show()
+        # blink a second time
+        time.sleep(delay)
         self.draw_eyes(wide_open=False)
         self.show()
         time.sleep(delay)
